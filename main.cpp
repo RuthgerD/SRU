f6efae8dd7848cbe2446b5e68a72f657ff639ee2
Ruthger Dijt <ruthgerdijt@gmail.com>
2020-01-14 10:22:52 +0100

    play around with optionals

--- main.cpp
+++ main.cpp
@@ -9,21 +9,28 @@
 static constexpr auto pattern = ctll::fixed_string{
     "BT\\n/F(.*?) (.*?) Tf\\n(.*?) Tm\\n\\((.*?)\\)Tj\\nET\\n"};
 
-std::string QFileRead(const char *path) {
-    auto f = std::fopen(path, "r");
-    std::fseek(f, 0, SEEK_END);
-    std::string str;
-    str.resize(std::ftell(f));
-    std::fseek(f, 0, SEEK_SET);
-    std::fread(str.data(), str.length(), 1, f);
-    std::fclose(f);
-    return str;
+std::optional<std::string> QFileRead(const char *path) {
+    if (auto f = std::fopen(path, "r")) {
+        std::fseek(f, 0, SEEK_END);
+        std::string str;
+        str.resize(std::ftell(f));
+        std::fseek(f, 0, SEEK_SET);
+        std::fread(str.data(), str.length(), 1, f);
+        std::fclose(f);
+        return str;
+    } else {
+        return {};
+    }
 }
 
 int main(int argc, char **argv) {
-    const std::string pdfdata = QFileRead("/home/ruthger/test.pdf");
+    const auto pdfdata = QFileRead("/home/ruthger/test.pdf");
 
-    for (auto found : ctre::range<pattern>(pdfdata)) {
-        std::cout << found.get<0>().to_view();
+    if (pdfdata.has_value()) {
+        for (auto found : ctre::range<pattern>(pdfdata.value())) {
+            std::cout << found.get<0>().to_view();
+        }
+    } else {
+        std::cout << "File not found :)" << std::endl;
     }
 }
