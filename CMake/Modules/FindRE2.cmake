c455249226bffb97181b2682f2239edd71c76635
Ruthger Dijt <ruthgerdijt@gmail.com>
2020-03-01 09:38:49 +0100

    Compile re2 if not found
    Signed-off-by: Ruthger Dijt <ruthgerdijt@gmail.com>

--- CMake/Modules/FindRE2.cmake
+++ CMake/Modules/FindRE2.cmake
@@ -9,5 +9,15 @@ IF (SYSTEM_RE2_INCLUDE_DIR)
     MESSAGE(ERROR "Found RE2 headers, but not the library")
   ENDIF ()
 ELSE ()
-  MESSAGE(STATUS "Did not find system RE2")
+  MESSAGE(STATUS "Did not find system RE2, compiling from source...")
+  SET(RE2_FOUND TRUE)
+  SET(RE2_INCLUDE_DIR ${CMAKE_BINARY_DIR}/re2)
+  SET(RE2_LIBRARY ${CMAKE_BINARY_DIR}/re2/obj/so/libre2.so)
+  add_custom_target(external_re2 ALL
+          COMMAND cd ${CMAKE_BINARY_DIR} && [ -f ${RE2_LIBRARIES} ] || git clone https://github.com/google/re2.git
+          COMMAND cd ${CMAKE_BINARY_DIR}/re2 && make
+          COMMAND echo ${CMAKE_BINARY_DIR}/re2
+          )
+  add_library(re2compil SHARED IMPORTED)
+  add_dependencies(re2compil external_re2)
 ENDIF ()
